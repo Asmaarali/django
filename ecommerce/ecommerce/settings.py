f8a3a9f58543e2cd66cb73bb20adcdb9f37d37b6
Asmaarali <asmaarali00000@gmail.com>
2023-09-20 18:13:29 +0500

    second commit

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # for Sending emails
 
 EMAIL_HOST = 'smtp.gmail.com'  # smtpout.secureserver.net
-EMAIL_HOST_USER = 'asmaarfuuast@gmail.com'
-EMAIL_HOST_PASSWORD = 'qcpzewhuvzvybhgz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
